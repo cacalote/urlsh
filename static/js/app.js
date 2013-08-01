1b98b5c72d812a68545b5265d8628e1400c64a21
Felipe cruz <felipecruz@loogica.net>
2013-08-01 17:07:03 -0300

    Remove domain from web module.
    Redirect '/' to loogica.html

--- static/js/app.js
+++ static/js/app.js
@@ -3,14 +3,17 @@ function Controller($scope, $http) {
   $scope.urls = {};
 
   $scope.saved = function(data, status, header, config) {
-    $scope.urls = data;
+    $scope.saved_shortened = data.shortened;
+    $scope.saved_url = data.url;
+    $scope.event = {};
+    $scope.event.url = "url here";
   };
- 
+
   $scope.save = function(_event) {
     $scope.master = angular.copy(_event);
     $http.post('/add_url/', $scope.master).success($scope.saved);
   };
- 
+
   $scope.reset = function() {
     $scope._event = angular.copy($scope.master);
     $scope.master= {};
@@ -18,6 +21,4 @@ function Controller($scope, $http) {
         $scope.urls = data;
     });
   };
- 
-  $scope.reset();
 }
--- web.py
+++ web.py
@@ -1,58 +1,24 @@
+import logging
 import re
 
+from decouple import Config
 from coopy.base import init_persistent_system
 from flask import Flask, abort, request, render_template, redirect, jsonify
 
+from domain import URLShortner
+
+log = logging.getLogger(__name__)
+
+config = Config('settings.ini')
 app = Flask(__name__)
-
-'''
-url regex from:
-http://stackoverflow.com/questions/7160737/python-how-to-validate-a-url-in-python-malformed-or-not
-'''
-url_regex = re.compile(
-    r'^(?:http|ftp)s?://' # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-    r'localhost|' #localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-    r'(?::\d+)?' # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
-class URLShortner(object):
-    def __init__(self):
-        self.seed = 37483299
-        self.urls = {}
-        self.count = 10000000000000000
-
-    def _create_url(self, uid):
-        '''
-            from: https://github.com/mrigor/url-shortener/blob/master/url_shortener.py
-        '''
-        HASH_BASE = 'lahcx9yV6OsuefCEvJAbd0SZIGWYFQtUB8KX5jqR4NMzH1PTg37npwrLimD2ko'
-        s = []
-        mod = len(HASH_BASE)
-        while uid:
-            uid, c = divmod(uid, mod)
-            s.append(HASH_BASE[c])
-        return ''.join(s)
-
-    def add_url(self, url):
-        if url_regex.match(url) == None:
-            raise Exception("Invalid URL")
-        self.count += self.seed
-        shortened = self._create_url(self.count)
-        self.urls[shortened] = url
-        return dict(shortened=shortened,
-                    url=url)
-
-    def resolve(self, _id):
-        return self.urls[_id]
-
+app.secret_key = config('SECRET_KEY').encode('utf-8')
+app.logger.addHandler(logging.StreamHandler())
 system = init_persistent_system(URLShortner())
-MAIN_SITE = "http://loogica.net"
+MAIN_SITE = "http://loogi.ca"
 
 @app.route('/')
 def main_site():
-    return redirect(MAIN_SITE)
+    return render_template('loogica.html')
 
 @app.route('/main/')
 def main():
